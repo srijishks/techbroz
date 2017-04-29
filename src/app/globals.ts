53555ef4662d6e63e5e7bcfaf86953b92425ccf5
Sreejish KS <Sreejish KS>
2017-04-29 18:16:19 +1200

    Extra design issue fix

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 Injectable()
 export class Globals{
 
-API_BASE_URL = "http://sreejish.com/json/techborz";
+ApiUrl = "http://sreejish.com/json/techborz";
+imageUrl = "http://techbroz.co.nz/images/";
     
 }
